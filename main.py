4b2ab7068131e725c795b8c465e1684d453055c7
goeva_elizaveta <liz.goeva02@mail.ru>
2023-06-26 16:02:32 +0300

    Created script for creating and saving video with running string

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+from math import ceil
+import cv2
+import numpy as np
+
+SIZE = (100, 100)
+VIDEO_DURATION = 3  # in seconds
+TEXT_STRING = 'Бегущая строка'
+FONT = cv2.FONT_HERSHEY_COMPLEX
+SCALE, THICKNESS = 3, 3
+TEXT_COLOR = (255, 255, 255)  # white
+
+
+def name_videofile(filename='running_line.mp4'):
+    """
+    Generates name for a video file
+    Gives unique name with number, if several videos were generated
+
+    :param filename: prefered filename, by default 'running_line.mp4'
+    :return: Resulting filename for saving
+    """
+    if os.path.exists(filename):
+        name = filename.split('.')[0]
+        file_num = 1
+        while True:
+            filename = f'{name}{file_num}.mp4'
+            if not os.path.exists(filename):
+                break
+            file_num += 1
+    return filename
+
+
+def create_video():
+    video_name = name_videofile()
+    if TEXT_STRING:
+        text_size = cv2.getTextSize(TEXT_STRING, FONT, SCALE, THICKNESS)[0]  # text size in pixels
+    else:
+        print('Строка не может быть пустой')
+        return
+
+    fps = 60
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(video_name, fourcc, fps, SIZE)
+
+    x = SIZE[0]
+    x_shift = ceil((SIZE[0] + text_size[0]) / fps / VIDEO_DURATION)
+    while x > -text_size[0]:
+        img = np.zeros((SIZE[1], SIZE[0]), dtype=np.uint8)
+        cv2.putText(img, TEXT_STRING, (x, 80), FONT, SCALE, TEXT_COLOR, THICKNESS)
+        cv2.imwrite('background.jpg', img)
+        img = cv2.imread('background.jpg')
+        out.write(img)
+        x -= x_shift
+
+    cv2.destroyAllWindows()
+    out.release()
+    print(f'Видео сохранено под именем файла {video_name}')
+    try:
+        os.remove('background.jpg')
+    except FileNotFoundError:
+        pass
+
+
+create_video()
